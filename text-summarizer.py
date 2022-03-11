63b7604081d4d891da0dc491a3faba70327b1d8a
Mikhail Murunov <haskiindahouse@gmail.com>
2022-03-11 10:55:43 +0300

    romove files

--- text-summarizer.py
+++ text-summarizer.py
@@ -1,87 +1,95 @@
 #!/usr/bin/env python
 # coding: utf-8
+import networkx
 import nltk
 from nltk.corpus import stopwords
 from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
- 
-def read_article(file_name):
-    file = open(file_name, "r")
-    filedata = file.readlines()
-    article = filedata[0].split(". ")
+import pandas as pd
+
+
+def read_data(file_name):
+    workbook = pd.read_excel(file_name)
+    return workbook["text"]
+
+
+def read_article(data):
+    article = data.split(". ")
     sentences = []
 
     for sentence in article:
-        print(sentence)
         sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
-    sentences.pop() 
-    
+    sentences.pop()
+
     return sentences
+
 
 def sentence_similarity(sent1, sent2, stopwords=None):
     if stopwords is None:
         stopwords = []
- 
+
     sent1 = [w.lower() for w in sent1]
     sent2 = [w.lower() for w in sent2]
- 
+
     all_words = list(set(sent1 + sent2))
- 
+
     vector1 = [0] * len(all_words)
     vector2 = [0] * len(all_words)
- 
-    # build the vector for the first sentence
+
     for w in sent1:
         if w in stopwords:
             continue
         vector1[all_words.index(w)] += 1
- 
-    # build the vector for the second sentence
+
     for w in sent2:
         if w in stopwords:
             continue
         vector2[all_words.index(w)] += 1
- 
+
     return 1 - cosine_distance(vector1, vector2)
- 
+
+
 def build_similarity_matrix(sentences, stop_words):
-    # Create an empty similarity matrix
     similarity_matrix = np.zeros((len(sentences), len(sentences)))
- 
+
     for idx1 in range(len(sentences)):
         for idx2 in range(len(sentences)):
-            if idx1 == idx2: #ignore if both are same sentences
-                continue 
+            if idx1 == idx2:
+                continue
             similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
 
     return similarity_matrix
 
 
-def generate_summary(file_name, top_n=5):
-    nltk.download("stopwords")
-    stop_words = stopwords.words('english')
+def generate_summary(data, stop_words, top_n=5):
     summarize_text = []
 
-    # Step 1 - Read text anc split it
-    sentences =  read_article(file_name)
+    sentences = read_article(data)
 
-    # Step 2 - Generate Similary Martix across sentences
     sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
-
     # Step 3 - Rank sentences in similarity martix
     sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
     scores = nx.pagerank(sentence_similarity_graph)
-
     # Step 4 - Sort the rank and pick top sentences
-    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
-    print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+    ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+
 
     for i in range(top_n):
-      summarize_text.append(" ".join(ranked_sentence[i][1]))
+        summarize_text.append(" ".join(ranked_sentence[i][1]))
+        # Step 5 - Offcourse, output the summarize texr
+    print("Краткая сведения по тексту: \n", ". ".join(summarize_text))
 
-    # Step 5 - Offcourse, output the summarize text
-    print("Summarize Text: \n", ". ".join(summarize_text))
 
-# let's begin
-generate_summary( "msft.txt", 2)
+def run_summarize_for_all(file_name):
+    nltk.download("stopwords")
+    stop_words = stopwords.words('russian')
+    data = read_data(file_name)
+    result = [
+        generate_summary(news, stop_words, 2)
+        for news in data
+    ]
+    return result
+
+df = pd.DataFrame({"Краткие сведения по новостям:": run_summarize_for_all('data.xlsx')})
+df.to_excel('news_summarize.xlsx', sheet_name='Суть', index=False)
